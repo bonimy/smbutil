f16321460fbc925c00a6d2a34efc841573191cc4
Nelson Garcia <ngarc017@ucr.edu>
2017-04-03 12:59:22 -0700

    APS Next Value fix
    Update value to reflect current string table

--- resrc1.h
+++ resrc1.h
@@ -351,7 +351,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        181
+#define _APS_NEXT_RESOURCE_VALUE        522
 #define _APS_NEXT_COMMAND_VALUE         40052
 #define _APS_NEXT_CONTROL_VALUE         2216
 #define _APS_NEXT_SYMED_VALUE           101
